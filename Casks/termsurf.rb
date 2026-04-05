7ffb3825919e2c94a88190645b88e95066bf3766
Ryan X. Charles <ryan@ryanxcharles.com>
2026-04-05 15:08:13 -0500

    Switch from Formula to Cask

---
+++ Casks/termsurf.rb
@@ -0,0 +1,30 @@
+cask "termsurf" do
+  version "0.1.0"
+  sha256 "536a9aa0c925985f2a95a5677c175dcf259062c0d8c8fafe202748de69b38088"
+
+  url "https://github.com/termsurf/termsurf/releases/download/v#{version}/termsurf-#{version}-aarch64-apple-darwin.tar.gz"
+  name "TermSurf"
+  desc "A protocol for embedding web browsers inside terminal emulators"
+  homepage "https://termsurf.com"
+
+  app "TermSurf Wezboard.app"
+  binary "web"
+  binary "wezboard"
+
+  artifact "roamium", target: "/opt/homebrew/opt/termsurf-roamium"
+
+  postflight do
+    system_command "codesign", args: ["--force", "--sign", "-", staged_path/"web"]
+    system_command "codesign", args: ["--force", "--sign", "-", staged_path/"wezboard"]
+    system_command "codesign", args: ["--force", "--sign", "-", "/opt/homebrew/opt/termsurf-roamium/roamium"]
+    system_command "codesign",
+                   args: ["--force", "--deep", "--sign", "-", "/Applications/TermSurf Wezboard.app"],
+                   print_stderr: false
+  end
+
+  zap trash: [
+    "~/.config/termsurf",
+    "~/.local/share/termsurf",
+    "~/.local/state/termsurf",
+  ]
+end
